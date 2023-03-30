670b0d9cc71afb9a25693db4dc9415a08f15a541
Oguzhan Ozturk <ozturk210@gmail.com>
2023-03-30 15:46:30 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Oguzhan Ozturk on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
